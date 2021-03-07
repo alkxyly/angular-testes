1e37e8a2410b9134ce840ebaeb4ea1b22f8c9752
alkxyly <alkxyly@hotmail.com>
2021-03-06 21:34:59 -0300

    02.02 Deixando o teste mais preciso

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -5,6 +5,6 @@ describe(UniqueIdService.name, () =>{
         const service = new UniqueIdService();
         const id = service.generetedUniqueIdWithPrefix('app');
 
-        expect(id).toContain('app-');
+        expect(id.startsWith('app-')).toBeTrue();
     });
 });
