caa492cabead87748a1105e0d596b8f4caccd083
alkxyly <alkxyly@hotmail.com>
2021-03-06 23:30:23 -0300

    02.03 gerando 50 ids diferentes

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -7,4 +7,18 @@ describe(UniqueIdService.name, () =>{
 
         expect(id.startsWith('app-')).toBeTrue();
     });
+
+    it(`#${UniqueIdService.prototype.generetedUniqueIdWithPrefix.name}  should not generate duplicate id`, ()=>{
+        const service = new UniqueIdService();
+        const ids = new Set();
+
+        for(let i = 0; i <  50; i++){
+            ids.add(service.generetedUniqueIdWithPrefix('app'));
+        }
+
+        expect(ids.size).toBe(50);
+
+    });
+
+    
 });
