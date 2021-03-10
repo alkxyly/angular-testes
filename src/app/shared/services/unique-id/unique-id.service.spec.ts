e7c3032a496924d5c5103035679d7eda3c94b3ec
alkxyly <alkxyly@hotmail.com>
2021-03-09 22:40:38 -0300

    02.05 function beforeEach

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,15 +1,24 @@
 import { UniqueIdService } from "./unique-id.service";
 
+
+
 describe(UniqueIdService.name, () =>{
+    
+    let service = null;
+    
+    beforeEach(() =>{
+        service  = new UniqueIdService();
+    });
+
     it(`#${UniqueIdService.prototype.generetedUniqueIdWithPrefix.name} should generate id when called with prefix`, () =>{
-        const service = new UniqueIdService();
+    
         const id = service.generetedUniqueIdWithPrefix('app');
 
         expect(id.startsWith('app-')).toBeTrue();
     });
 
     it(`#${UniqueIdService.prototype.generetedUniqueIdWithPrefix.name}  should not generate duplicate id`, ()=>{
-        const service = new UniqueIdService();
+        
         const ids = new Set();
 
         for(let i = 0; i <  50; i++){
@@ -17,6 +26,15 @@ describe(UniqueIdService.name, () =>{
         }
 
         expect(ids.size).toBe(50);
+
+    });
+
+    it(`#${UniqueIdService.prototype.generetedUniqueIdWithPrefix.name}  should return the number id  generated`, ()=>{
+       
+        service.generetedUniqueIdWithPrefix('app');
+        service.generetedUniqueIdWithPrefix('app');
+
+        expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
 
     });
 
