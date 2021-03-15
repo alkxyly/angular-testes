6ee55932317fa6ec9381e51a753325ffc816d491
alkxyly <alkxyly@hotmail.com>
2021-03-15 13:02:36 -0300

    03.03 Primeiro component

---
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LikeWidgetComponent } from './like-widget.component';
+
+@NgModule({
+    declarations: [
+        LikeWidgetComponent
+    ],
+    imports: [
+        CommonModule,
+        FontAwesomeModule
+    ],
+    exports:[]
+})
+export class LikeWidgetModule{
+
+}
